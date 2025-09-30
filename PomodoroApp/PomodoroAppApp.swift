51cef524dc8dbd5c415647db6214f556ad862229
Ashraful Alam <ashraful9194@gmail.com>
2025-09-30 19:56:08 +0600

    Initial Commit

---
+++ PomodoroApp/PomodoroAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroAppApp.swift
+//  PomodoroApp
+//
+//  Created by Ashraful Alam on 9/30/25.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
